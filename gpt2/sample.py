d4f6d0ae81b1cfcc999066265f274d8bc01da5d2
vincrichard <vincent.richard@elix-inc.com>
2024-10-28 17:03:46 +0900

    [Update] training and smapling scripts

--- gpt2/sample.py
+++ gpt2/sample.py
@@ -1,47 +1,55 @@
 """
 Sample from a trained model
 """
+
 import os
-import pickle
 from contextlib import nullcontext
 import torch
 from model import GPTConfig, GPT
 
+from protein_sequence.gpt2.dataset import ProteinSequenceDataset
+
+
+datasets = {"protein_sequence": ProteinSequenceDataset}
+dataset_params = {}
+dataset = ""
 # -----------------------------------------------------------------------------
-init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-out_dir = 'out' # ignored if init_from is not 'resume'
-start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
-temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
-top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
+init_from = "resume"  # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+out_dir = "out"  # ignored if init_from is not 'resume'
+start = "\n"  # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+num_samples = 10  # number of samples to draw
+max_new_tokens = 500  # number of tokens generated in each sample
+temperature = 0.8  # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+top_k = 200  # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
-device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
-compile = False # use PyTorch 2.0 to compile the model to be faster
-exec(open('gpt2/configurator.py').read()) # overrides from command line or config file
+device = "cuda"  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+dtype = (
+    "bfloat16" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else "float16"
+)  # 'float32' or 'bfloat16' or 'float16'
+compile = False  # use PyTorch 2.0 to compile the model to be faster
+exec(open("gpt2/configurator.py").read())  # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
-torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
-torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
-ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
+device_type = "cuda" if "cuda" in device else "cpu"  # for later use in torch.autocast
+ptdtype = {"float32": torch.float32, "bfloat16": torch.bfloat16, "float16": torch.float16}[dtype]
+ctx = nullcontext() if device_type == "cpu" else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # model
 
 # init from a model saved in a specific directory
-ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+ckpt_path = os.path.join(out_dir, "ckpt.pt")
 checkpoint = torch.load(ckpt_path, map_location=device)
-gptconf = GPTConfig(**checkpoint['model_args'])
+gptconf = GPTConfig(**checkpoint["model_args"])
 model = GPT(gptconf)
-state_dict = checkpoint['model']
-unwanted_prefix = '_orig_mod.'
-for k,v in list(state_dict.items()):
+state_dict = checkpoint["model"]
+unwanted_prefix = "_orig_mod."
+for k, v in list(state_dict.items()):
     if k.startswith(unwanted_prefix):
-        state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+        state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
 model.load_state_dict(state_dict)
 
 model.eval()
@@ -49,13 +57,14 @@ model.to(device)
 
 tokenizer = Tokenizer()
 
-data_dir = os.path.join("gpt2", 'data', dataset)
-data = torch.load(os.path.join(data_dir, 'train.pt'))
 
-ix = torch.randint(len(data), (10,))
-inputs = data[ix]
+training_data = datasets[dataset](**dataset_params, split="train")
 
-if device_type == 'cuda':
+
+ix = torch.randint(len(training_data), (10,))
+inputs = torch.stack([training_data[int(i)] for i in ix])
+
+if device_type == "cuda":
     inputs = inputs.pin_memory().to(device, non_blocking=True)
 else:
     inputs = inputs.to(device)
@@ -67,4 +76,4 @@ with torch.no_grad():
             y = model.generate(x[:256].unsqueeze(0), max_new_tokens, temperature=temperature, top_k=top_k)
             # y = model.generate(x[:256][None, ...], max_new_tokens, temperature=temperature, top_k=top_k)
             print(tokenizer.decode(y[0].tolist()))
-            print('---------------')
+            print("---------------")
